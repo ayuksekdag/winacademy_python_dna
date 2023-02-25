a46cc8c5191d5203cc34895b902185607563f16c
Ali Ihsan Yuksekdag <ayuksekdag@gmail.com>
2023-02-25 23:53:24 +0100

    1st commit

---
+++ functions/main.py
@@ -0,0 +1,33 @@
+# Do not modify these lines
+__winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
+__human_name__ = 'functions'
+
+# Add your code after this line
+
+
+def greet(name):
+    return f"Hello {name}"
+
+
+print(greet("Ali Ihsan"))
+
+
+def add(one, two, three):
+    return one + two + three
+
+
+print(add(1, 2, 3))
+
+
+def positive(number):
+    return number > 0
+
+
+print(positive(-1))
+
+
+def negative(number):
+    return number < 0
+
+
+print(negative(-1))
